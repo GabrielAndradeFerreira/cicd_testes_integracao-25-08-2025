4131b12ac83c6ae3887e0617a067da2e7738871a
GabrielAndradeFerreira <80432575+GabrielAndradeFerreira@users.noreply.github.com>
2025-08-26 00:49:50 +0000

    repositório com os primeiros testes

---
+++ testes/math.test.js
@@ -0,0 +1,16 @@
+const math = require('../math');
+test('soma 2 + 3 igual a 5', () => {
+    expect(math.add(2, 3)).toBe(5);
+});
+
+test('soma 2 - 3 igual a 1', () => {
+    expect(math.subtract(2, 3)).toBe(1);
+});
+
+test('soma 2 * 3 igual a 6', () => {
+    expect(math.multiply(2, 3)).toBe(6);
+});
+
+test('soma 2 / 3 igual a 5', () => {
+    expect(math.divide(2, 3)).toBe(0.6666666666666666);
+});
